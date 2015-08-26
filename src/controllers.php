3d411b620c53d4fce845491dc09a61751a4d654d
Guillermo Zeleny <guillermo@spraystudio.com.mx>
2015-08-26 10:56:07 -0500

    Controller Payment

--- src/controllers.php
+++ src/controllers.php
@@ -13,7 +13,48 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 $app->get('/', function () use ($app) {
             $lang = $app['session']->get('current_language') ? $app['session']->get('current_language') : 'es';
-            $payment = (object) array('number' => 100, 'string' => '$100.00 MXN');
+
+            $dolar = 19;
+            $euro = 21;
+            $libra = 27;
+
+            switch ($lang) {
+                case "es":
+                    $payment = (object) array('number' => 12 * $dolar, 'string' => '$12.00 USD');
+                    break;
+                case "asia":
+                    $payment = (object) array('number' => 10 * $dolar, 'string' => '$10.00 USD');
+                    break;
+                case "de":
+                    $payment = (object) array('number' => 14 * $euro, 'string' => '$14.00 EUR');
+                    break;
+                case "en":
+                    $payment = (object) array('number' => 14 * $dolar, 'string' => '$10.00 USD');
+                    break;
+                case "eu":
+                    $payment = (object) array('number' => 10 * $euro, 'string' => '$10.00 EUR');
+                    break;
+                case "fr":
+                    $payment = (object) array('number' => 9 * $euro, 'string' => '$9.00 EUR');
+                    break;
+                case "in":
+                    $payment = (object) array('number' => 7 * $euro, 'string' => '$7.00 USD');
+                    break;
+                case "it":
+                    $payment = (object) array('number' => 11 * $euro, 'string' => '$11.00 EUR');
+                    break;
+                case "nl":
+                    $payment = (object) array('number' => 14 * $euro, 'string' => '$14.00 EUR');
+                    break;
+                case "pt":
+                    $payment = (object) array('number' => 11 * $euro, 'string' => '$11.00 EUR');
+                    break;
+                case "uk":
+                    $payment = (object) array('number' => 14 * $libra, 'string' => '$14.00 GBP');
+                    break;
+            }
+
+
             return $app['twig']->render('index.html.twig', array('language' => $lang, 'payment' => $payment));
         })
         ->bind('homepage')
