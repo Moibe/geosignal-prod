3bd001a7f2b1c93729b2883dcb691c6a51c6259c
jorge serrano <alberto@spraystudio.com.mx>
2016-01-14 18:02:51 -0600

    add lenguajes trans

--- src/Geosignal/WebBundle/Controller/DefaultController.php
+++ src/Geosignal/WebBundle/Controller/DefaultController.php
@@ -7,22 +7,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use \Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      * @Template("GeosignalWebBundle::index.html.twig")
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         return array();
     }
+
     /**
      * @Route("/output-success", name="result")
      * @Template("GeosignalWebBundle::result.html.twig")
      */
-    public function resultAction()
-    {
-        return array();
+    public function resultAction() {
+        return $_COOKIE['result_showed'] == "false" && isset($_COOKIE['user_latitude']) && isset($_COOKIE['user_longitude']) ? array() : $this->redirect($this->generateUrl('homepage'));
     }
+
 }
