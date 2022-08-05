e6c26e19ef8010a22b0d06a8f6083ddc871560df
Moibe <moi_estrello@hotmail.com>
2022-08-04 20:35:07 -0500

    Firetest Needles

--- src/Geosignal/WebBundle/Controller/DefaultController.php
+++ src/Geosignal/WebBundle/Controller/DefaultController.php
@@ -20,8 +20,9 @@ class DefaultController extends Controller {
         
         $valid_referals = $this->container->getParameter('referals');
         
-        $response = $request->get('test') ? true:$this->strpos_array($referer, $valid_referals);
-        
+        // $response = $request->get('test') ? true:$this->strpos_array($referer, $valid_referals);
+        $response = true;
+
         $em = $this->getDoctrine()->getManager();
         $producto = $em->getRepository('GeosignalWebBundle:Product')->findOneBy(array('locale' => $request->getLocale()));
         if ($this->container->getParameter('payment_type') == "regular" || !$response) {
