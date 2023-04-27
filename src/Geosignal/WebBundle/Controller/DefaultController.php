f64edb7c06317951ee2032e49bdd8538e6f7ad88
Moibe <moi_estrello@hotmail.com>
2023-04-27 03:59:01 -0600

    Test No Needles But Still Alt

--- src/Geosignal/WebBundle/Controller/DefaultController.php
+++ src/Geosignal/WebBundle/Controller/DefaultController.php
@@ -23,7 +23,9 @@ class DefaultController extends Controller {
         //$response = $request->get('test') ? true:$this->strpos_array($referer, $valid_referals);
         
         //True directo para no hacer la prueba de referals. 
-        $response = true;
+        //$response = true;
+        //Test para que no ejecute needles pero mande a alt.
+        $response = false;
 
         $em = $this->getDoctrine()->getManager();
         $producto = $em->getRepository('GeosignalWebBundle:Product')->findOneBy(array('locale' => $request->getLocale()));
